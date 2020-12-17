5641d2cf134e63b6c3da45b2a668b5a79e2703c9
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-12-17 23:11:18 +0100

    Fix manifests export.

--- .k8s/components/app.ts
+++ .k8s/components/app.ts
@@ -2,38 +2,35 @@ import env from "@kosko/env"
 
 import config from "../../socialgouv.json"
 
-import app from "@socialgouv/kosko-charts/components/app"
-import nginx from "@socialgouv/kosko-charts/components/nginx"
+import * as app from "@socialgouv/kosko-charts/components/app"
+import * as nginx from "@socialgouv/kosko-charts/components/nginx"
 
 const { type } = config
 
-if (type === "app") {
-  const { create } = app
+const { create } = type === "app" ? app : nginx
 
-  const manifests = create("app", {
-    env,
-    config: { containerPort: 3000 },
-    deployment: {
-      container: {
-        resources: {
-          requests: {
-            cpu: "50m",
-            memory: "128Mi",
-          },
-          limits: {
-            cpu: "200m",
-            memory: "256Mi",
+const manifests = create(
+  "app",
+  type === "app"
+    ? {
+        env,
+        config: { containerPort: 3000 },
+        deployment: {
+          container: {
+            resources: {
+              requests: {
+                cpu: "50m",
+                memory: "128Mi",
+              },
+              limits: {
+                cpu: "200m",
+                memory: "256Mi",
+              },
+            },
           },
         },
-      },
-    },
-  })
-} else {
-  const { create } = nginx
-
-  const manifests = create("app", {
-    env,
-  })
-}
+      }
+    : { env }
+)
 
 export default manifests
