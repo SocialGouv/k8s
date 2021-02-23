1aeb297f4e88a3500045704f1274d547440d8681
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-02-23 15:24:18 +0100

    fix: Add APP_BASE_URL env var. (#18)

--- .k8s/components/app.ts
+++ .k8s/components/app.ts
@@ -1,5 +1,11 @@
+import { ok } from "assert"
 import env from "@kosko/env"
+import { EnvVar } from "kubernetes-models/v1/EnvVar"
+import { addEnv } from "@socialgouv/kosko-charts/utils/addEnv"
 import { create } from "@socialgouv/kosko-charts/components/app"
+import { Deployment } from "kubernetes-models/apps/v1/Deployment"
+import { getIngressHost } from "@socialgouv/kosko-charts/utils/getIngressHost"
+import { getManifestByKind } from "@socialgouv/kosko-charts/utils/getManifestByKind"
 import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath"
 
 import Config from "../utils/config"
@@ -27,26 +33,43 @@ export default () => {
       )
     : {}
 
-  return type && type === "app"
-    ? create(name, {
-        env,
-        config: { containerPort: 3000 },
-        deployment: {
-          image: getHarborImagePath({ name }),
-          container: {
-            resources: {
-              requests: {
-                cpu: "50m",
-                memory: "128Mi",
-              },
-              limits: {
-                cpu: "200m",
-                memory: "256Mi",
-              },
+  if (type && type === "app") {
+    const manifests = create(name, {
+      env,
+      config: { containerPort: 3000 },
+      deployment: {
+        image: getHarborImagePath({ name }),
+        container: {
+          resources: {
+            requests: {
+              cpu: "50m",
+              memory: "128Mi",
             },
-            ...probes,
+            limits: {
+              cpu: "200m",
+              memory: "256Mi",
+            },
           },
+          ...probes,
         },
-      })
-    : []
+      },
+    })
+
+    /* pass dynamic deployment URL as env var to the container */
+    //@ts-expect-error
+    const deployment = getManifestByKind(manifests, Deployment) as Deployment
+
+    ok(deployment)
+
+    const frontendUrl = new EnvVar({
+      name: "APP_BASE_URL",
+      value: `https://${getIngressHost(manifests)}`,
+    })
+
+    addEnv({ deployment, data: frontendUrl })
+
+    return manifests
+  } else {
+    return []
+  }
 }
