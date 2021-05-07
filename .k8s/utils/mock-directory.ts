31afa650131e479499cafe53c3096b0cd66e305e
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-05-07 17:08:51 +0200

    Update snapshots.

--- .k8s/utils/mock-directory.ts
+++ .k8s/utils/mock-directory.ts
@@ -7,7 +7,7 @@ export default () => {
   const cwd = directory();
 
   fs.symlinkSync(path.join(__dirname, "..", "utils"), `${cwd}/utils`)
-  fsx.copy(path.join(__dirname, "..", "components"), `${cwd}/components`)
+  fsx.copySync(path.join(__dirname, "..", "components"), `${cwd}/components`)
   fs.symlinkSync(path.join(__dirname, "..", "kosko.toml"), `${cwd}/kosko.toml`)
   fs.symlinkSync(path.join(__dirname, "..", "node_modules"), `${cwd}/node_modules`)
   fs.symlinkSync(path.join(__dirname, "..", "tsconfig.json"), `${cwd}/tsconfig.json`)
