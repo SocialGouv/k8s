88aa77f8d9559a2cf516badba5c873f57b9caa84
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-05-27 18:09:47 +0200

    feat: Expose Hasura component from config.

--- .k8s/__tests__/hasura/kosko generate --env preprod.ts
+++ .k8s/__tests__/hasura/kosko generate --env preprod.ts
@@ -6,11 +6,21 @@ import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env"
 const name = "myapp"
 
 jest.setTimeout(1000 * 60)
+
 test("hasura: kosko generate --preprod", async () => {
   expect(
     await getEnvManifests("preprod", "", {
       ...project(name).preprod,
       SOCIALGOUV_CONFIG_PATH: __dirname + "/config.json",
+    })
+  ).toMatchSnapshot()
+})
+
+test("hasura with ingress: kosko generate --preprod", async () => {
+  expect(
+    await getEnvManifests("preprod", "", {
+      ...project(name).preprod,
+      SOCIALGOUV_CONFIG_PATH: __dirname + "/config-with-ingress.json",
     })
   ).toMatchSnapshot()
 })
