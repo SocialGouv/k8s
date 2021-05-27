390bc5040a24c4ca3b41fd3c450fe99b1eeced82
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-05-27 18:31:15 +0200

    feat: Add registry option. (#35)

---
+++ .k8s/__tests__/registry/kosko generate --env dev.ts
@@ -0,0 +1,15 @@
+//
+
+import { getEnvManifests } from "@socialgouv/kosko-charts/testing"
+import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env"
+
+jest.setTimeout(1000 * 60)
+
+test("registry: kosko generate --dev", async () => {
+  expect(
+    await getEnvManifests("dev", "", {
+      ...project("myapp").dev,
+      SOCIALGOUV_CONFIG_PATH: __dirname + "/config.json",
+    })
+  ).toMatchSnapshot()
+})
