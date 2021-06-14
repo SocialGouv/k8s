95b5e005222dddc8f3b141ccd6e574f7f7705cc3
Douglas DUTEIL <douglasduteil@gmail.com>
2021-06-14 13:10:21 +0200

    feat(components): use async kosko api

--- .k8s/components/hasura.ts
+++ .k8s/components/hasura.ts
@@ -1,19 +1,18 @@
-import env from "@kosko/env"
-import { create } from "@socialgouv/kosko-charts/components/hasura"
-import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath"
-
-import Config from "../utils/config"
+import env from "@kosko/env";
+import { create } from "@socialgouv/kosko-charts/components/hasura";
+import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
+import Config from "../utils/config";
 
 export default () => {
-  const { name, hasura } = Config()
+  const { name, hasura } = Config();
 
   return hasura
-    ? create({
+    ? create("hasura", {
         env,
         config: { ingress: hasura === "exposed" },
         deployment: {
           image: getHarborImagePath({ name: `${name}-hasura` }),
         },
       })
-    : []
-}
+    : [];
+};
